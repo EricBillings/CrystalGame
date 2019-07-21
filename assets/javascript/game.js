97167f13251414cf4761f911849025d97dcfd4fd
Eric Billings <billings.company@gmail.com>
2019-07-21 15:38:02 -0400

    add crystal functionality and scoring function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,16 +8,47 @@ let crystalNumThree = Math.floor(Math.random() * 12) + 1;
 console.log(crystalNumThree);
 let crystalNumFour = Math.floor(Math.random() * 12) + 1;
 console.log(crystalNumFour);
+let userCount = 0;
 
-$("#crystalOne").click(function() {
+
+function compare(x, y) {
+    if (x === y) {
+        console.log("WIN");
+    } else if
+        (x < y) {
+        console.log("OK STATE");
+    } else {
+        console.log("LOSS");
+    }
+}
+
+
+$("#crystalOne").click(function () {
     console.log('Click 1 = ' + crystalNumOne);
+    userCount += crystalNumOne;
+    console.log(userCount);
+    compare(userCount, randNum)
 })
-$("#crystalTwo").click(function() {
+$("#crystalTwo").click(function () {
     console.log('Click 2 = ' + crystalNumTwo);
+    userCount += crystalNumTwo;
+    console.log(userCount);
+    compare(userCount, randNum)
+
 })
-$("#crystalThree").click(function() {
+$("#crystalThree").click(function () {
     console.log('Click 3 = ' + crystalNumThree);
+    userCount += crystalNumThree;
+    console.log(userCount);
+    compare(userCount, randNum)
+
 })
-$("#crystalFour").click(function() {
+$("#crystalFour").click(function () {
     console.log('Click 4 = ' + crystalNumFour);
+    userCount += crystalNumFour;
+    console.log(userCount);
+    compare(userCount, randNum)
+
 })
+
+
